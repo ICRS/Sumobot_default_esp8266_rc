c447fd292cef5239580a7e0fe8b0c684cfe461f2
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2022-06-09 11:57:10 +0100

    removed redundant files

--- src/src.ino
+++ src/src.ino
@@ -10,15 +10,11 @@ To use your device as the remote, connect to the SSID, type 192.168.1.1 - the ga
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 //include webpages
-#include "MAIN_PAGE.h"
-#include "CONTROL_1.h"
-#include "CONTROL_2.h"
-#include "CONTROL_3.h"
-#include "CONTROL_custom.h"
+#include "CONTROL.h"
 
-/* Put your SSID & Password */
-const char* ssid = "RC Car";  // SSID here
-const char* password = "12345678";  //Enter Password here
+/* Put your SSID & PASSWORD */
+const char* SSID = "RC Car";  // SSID here
+const char* PASSWORD = "12345678";  //Enter PASSWORD here
 
 /* Put IP Address details */
 IPAddress staticIP(192,168,1,1);
@@ -35,7 +31,6 @@ const byte LEDLB = D5;
 unsigned int LEDLeftint = 0; //LEFT value wheel control
 
 bool LEDLFbit;  //sets direction motor spins variables (refer to L298m H bridge)
-//WiFiClient client; 
 
 //Right control
 const byte LEDRight = D1;
@@ -48,9 +43,9 @@ bool LEDRFbit;
 
 
 void setup() {
+
+  // Init Serial - Debug
   Serial.begin(115200);
-  //WiFi.setOutputPower()
-  //client.setNoDelay(1);
   
   // code starts here
   //set pins to outputs
@@ -66,7 +61,7 @@ void setup() {
 
   
   //set up wifi stuff
-  WiFi.softAP(ssid, password, 6, false, 1); // channel (3rd entry) doesn't need to be 0, int at end sets max numb of stations (devices)
+  WiFi.softAP(SSID, PASSWORD, 6, false, 1);
   WiFi.softAPConfig(staticIP, gateway, subnet);
 
   //sets up what server does at each url 
@@ -76,7 +71,6 @@ void setup() {
   server.on("/control_1", handle_control_1);
   server.on("/control_2", handle_control_2);
   server.on("/control_3", handle_control_3);
-  server.on("/control_custom", handle_control_custom);
     
   server.onNotFound(handle_NotFound);
   server.begin(); //start server
@@ -93,112 +87,96 @@ void loop() {
   webSocket.loop();
   server.handleClient();  
     
-  //setting LED left and right values
+  // setting LED left and right values
   digitalWrite(LEDLF,LEDLFbit);
   digitalWrite(LEDLB,!(LEDLFbit));
   
   digitalWrite(LEDRF,LEDRFbit);
   digitalWrite(LEDRB,!(LEDRFbit));
 
-  //set PWM values
+  // set PWM values
   analogWrite(LEDLeft, LEDLeftint);
   analogWrite(LEDRight, LEDRightint); 
 
   if(WiFi.softAPgetStationNum()==0){
     LEDLeftint = 0;
     LEDRightint = 0;
-    } 
+  } 
 }
 
 void handle_OnConnect(){    /* setup on connect to 192.168.1.1 */
-  server.send(200,"text/html", MAIN_PAGE);
-  }
+  server.send(200,"text/html", CONTROL);
+}
 
 void handle_NotFound(){   /* if connect to not 192.168.1.1*/
   server.sendHeader("Location","/", true);
   server.send(302);
-  }
-
-void handle_control_1(){
-  server.send(200, "text/html", CONTROL_1);
-  }
-
-void handle_control_2(){
-  server.send(200, "text/html", CONTROL_2);
-  }
-
-void handle_control_3(){
-  server.send(200, "text/html", CONTROL_3);
-  }
-
-
-void handle_control_custom(){
-  server.send(200, "text/html", CONTROL_custom);
-  }
+}
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
 {
-  switch(type) {
+  switch(type) 
+  {
       
-        case WStype_DISCONNECTED:
-            Serial.println("Client disconnnected !"+ num);
-            break;
-            
-        case WStype_CONNECTED:
-            {
-                IPAddress ip = webSocket.remoteIP(num);
-                Serial.println("Websocket client connnected !");        
-                // send message to client
-                webSocket.sendTXT(num, "Connected");                
-            }
-            break;
-            
-        case WStype_TEXT:
-        String parse_payload;
-        for(int i = 0; i < length;  i++){
-            parse_payload.concat((char) payload[i]);
-          
-            }
-        Serial.println(parse_payload);
+    case WStype_DISCONNECTED:
+      Serial.println("Client disconnnected !"+ num);
+      break;
+        
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.println("Websocket client connnected !");        
+        // send message to client
+        webSocket.sendTXT(num, "Connected");                
+      }
+      break;
+        
+    case WStype_TEXT:
+      String parse_payload;
+      
+      for(int i = 0; i < length;  i++){
+        parse_payload.concat((char) payload[i]);
+      }
 
-        if(String(parse_payload[0]) == "l"){
-          int x = parser_x(parse_payload);
-          int y = parser_y(parse_payload);
+      Serial.println(parse_payload);
 
-          Serial.print(x); Serial.print(",");Serial.println(y);
-          
-          LEDLeftint = abs(x)*200;
-          LEDRightint = abs(y)*200;
+      if(String(parse_payload[0]) == "l"){
+        int x = parser_x(parse_payload);
+        int y = parser_y(parse_payload);
 
-          Serial.println(LEDLeftint);
-          Serial.println(LEDRightint);
-          if(x <0){
-            LEDLFbit = 0;
-            }
+        Serial.print(x); Serial.print(","); Serial.println(y);
+        
+        LEDLeftint = abs(x)*200;
+        LEDRightint = abs(y)*200;
 
-          else{
-            LEDLFbit = 1;
-            }
-
-          if(y<0){
-            LEDRFbit = 0;
-            }
-
-          if(y>=0){
-            LEDRFbit = 1;
-            }
-          }
+        Serial.println(LEDLeftint); Serial.println(LEDRightint);
+        if(x <0){
+          LEDLFbit = 0;
         }
+
+        else{
+          LEDLFbit = 1;
+        }
+
+        if(y<0){
+          LEDRFbit = 0;
+        }
+
+        if(y>=0){
+          LEDRFbit = 1;
+        }
+      }
+  }
 }
 
 int parser_x(String data){
   data.remove(data.indexOf("r"));
   data.remove(data.indexOf("l"),1);
   return data.toInt();
-  }
+}
 
 int parser_y(String data){
   data.remove(0,data.indexOf("r")+1);
   data.remove(data.indexOf("r"),1);
   return data.toInt();
-  }
+}
